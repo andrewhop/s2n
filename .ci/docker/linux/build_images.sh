7a16198e1207ce63d2d44a3f06fac1c7cf9ac39c
Andrew Hopkins <andhop@amazon.com>
2019-11-18 15:22:15 -0800

    Fix docker path

--- .ci/docker/linux/build_images.sh
+++ .ci/docker/linux/build_images.sh
@@ -12,5 +12,5 @@
 # implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
-docker build -m6g -t ubuntu-19.04:gcc-8x ubuntu-19.04_gcc-8x
-docker build -m6g -t ubuntu-19.04:clang-8x ubuntu-19.04_clang-8x
+docker build -m6g -t ubuntu-19.04:gcc-8x ubuntu-19.04_gcc-8x .
+docker build -m6g -t ubuntu-19.04:clang-8x ubuntu-19.04_clang-8x .
