b0e883cd035065f14e0aff58e252f41840ddc827
Andrew Hopkins <andhop@amazon.com>
2021-02-18 00:51:21 -0800

    Cleanup shared setup code

--- tests/benchmark/s2n_handshake_benchmark.cc
+++ tests/benchmark/s2n_handshake_benchmark.cc
@@ -28,19 +28,23 @@ extern "C" {
 #include "stuffer/s2n_stuffer.h"
 }
 
-
-
-/* These KEX's do not need a KEM configured */
-static const struct s2n_kex simple_kex[] = {
-    s2n_rsa,
-    s2n_dhe,
-    s2n_ecdhe,
+struct test_case {
+    const struct s2n_ecc_named_curve *curve_to_test;
+    const struct s2n_kem *kem_to_test;
+    struct s2n_cipher_suite *cipher_suite_to_test;
 };
 
-/* These KEX's need a KEM to be configured to succeed */
-static const struct s2n_kex kem_kex[] = {
-    s2n_kem,
-    s2n_hybrid_ecdhe_kem,
+static const struct test_case tests[] = {
+    {nullptr, nullptr, &s2n_dhe_rsa_with_aes_256_gcm_sha384},
+    {&s2n_ecc_curve_secp256r1, nullptr, &s2n_ecdhe_rsa_with_aes_256_gcm_sha384},
+    {&s2n_ecc_curve_secp384r1, nullptr, &s2n_ecdhe_rsa_with_aes_256_gcm_sha384},
+    {&s2n_ecc_curve_secp521r1, nullptr, &s2n_ecdhe_rsa_with_aes_256_gcm_sha384},
+    {&s2n_ecc_curve_x25519, nullptr, &s2n_ecdhe_rsa_with_aes_256_gcm_sha384},
+    {&s2n_ecc_curve_secp256r1, &s2n_sike_p503_r1, &s2n_ecdhe_sike_rsa_with_aes_256_gcm_sha384},
+    {&s2n_ecc_curve_secp256r1, &s2n_sike_p434_r2, &s2n_ecdhe_sike_rsa_with_aes_256_gcm_sha384},
+    {&s2n_ecc_curve_secp256r1, &s2n_bike1_l1_r1, &s2n_ecdhe_bike_rsa_with_aes_256_gcm_sha384},
+    {&s2n_ecc_curve_secp256r1, &s2n_bike1_l1_r2, &s2n_ecdhe_bike_rsa_with_aes_256_gcm_sha384},
+    {&s2n_ecc_curve_secp256r1, &s2n_kyber_512_r2, &s2n_ecdhe_kyber_rsa_with_aes_256_gcm_sha384},
 };
 
 static char *server_dhparams_pem = nullptr;
@@ -51,32 +55,42 @@ static struct s2n_cert_chain_and_key *server_chain_and_key = nullptr;
 static struct s2n_config *server_config = nullptr;
 struct s2n_connection *client_conn;
 struct s2n_connection *server_conn;
+struct s2n_blob client_asn1der = {0};
 
 static int one_time_setup() {
-    GUARD_NONNULL(server_dhparams_pem = (char*) malloc(S2N_MAX_TEST_PEM_SIZE));
-    GUARD(s2n_read_test_pem(S2N_DEFAULT_TEST_DHPARAMS, server_dhparams_pem, S2N_MAX_TEST_PEM_SIZE));
-
+    /* Server setup */
+    GUARD_NONNULL(server_conn = s2n_connection_new(S2N_SERVER));
+    GUARD_NONNULL(server_config = s2n_config_new());
+    GUARD(s2n_connection_set_config(server_conn, server_config));
 
     GUARD_NONNULL(server_cert_chain = (char*) malloc(S2N_MAX_TEST_PEM_SIZE));
     GUARD_NONNULL(server_private_key = (char*) malloc(S2N_MAX_TEST_PEM_SIZE));
     GUARD_NONNULL(client_chain = (char*) malloc(S2N_MAX_TEST_PEM_SIZE));
-
-
     GUARD(s2n_read_test_pem(S2N_RSA_2048_PKCS1_CERT_CHAIN, server_cert_chain, S2N_MAX_TEST_PEM_SIZE));
     GUARD(s2n_read_test_pem(S2N_RSA_2048_PKCS1_KEY, server_private_key, S2N_MAX_TEST_PEM_SIZE));
     GUARD(s2n_read_test_pem(S2N_RSA_2048_PKCS1_LEAF_CERT, client_chain, S2N_MAX_TEST_PEM_SIZE));
-
     GUARD_NONNULL(server_chain_and_key = s2n_cert_chain_and_key_new());
     GUARD(s2n_cert_chain_and_key_load_pem(server_chain_and_key, server_cert_chain, server_private_key));
-
-    GUARD_NONNULL(server_config = s2n_config_new());
-    GUARD(s2n_config_add_dhparams(server_config, server_dhparams_pem));
     GUARD(s2n_config_add_cert_chain_and_key_to_store(server_config, server_chain_and_key));
 
-    GUARD_NONNULL(client_conn = s2n_connection_new(S2N_CLIENT));
-    GUARD_NONNULL(server_conn = s2n_connection_new(S2N_SERVER));
-    GUARD(s2n_connection_set_config(server_conn, server_config));
+    GUARD_NONNULL(server_dhparams_pem = (char*) malloc(S2N_MAX_TEST_PEM_SIZE));
+    GUARD(s2n_read_test_pem(S2N_DEFAULT_TEST_DHPARAMS, server_dhparams_pem, S2N_MAX_TEST_PEM_SIZE));
+    GUARD(s2n_config_add_dhparams(server_config, server_dhparams_pem));
 
+    /* Client setup */
+    GUARD_NONNULL(client_conn = s2n_connection_new(S2N_CLIENT));
+
+    DEFER_CLEANUP(struct s2n_stuffer certificate_in = {0}, s2n_stuffer_free);
+    GUARD(s2n_stuffer_alloc(&certificate_in, S2N_MAX_TEST_PEM_SIZE));
+    DEFER_CLEANUP(struct s2n_stuffer certificate_out = {0}, s2n_stuffer_free);
+    GUARD(s2n_stuffer_alloc(&certificate_out, S2N_MAX_TEST_PEM_SIZE));
+
+    GUARD(s2n_blob_init(&client_asn1der, (uint8_t *) client_chain, strlen(client_chain) + 1));
+    GUARD(s2n_stuffer_write(&certificate_in, &client_asn1der));
+    GUARD(s2n_stuffer_certificate_from_pem(&certificate_in, &certificate_out));
+
+    client_asn1der.size = s2n_stuffer_data_available(&certificate_out);
+    GUARD(s2n_stuffer_read(&certificate_out, &client_asn1der));
 
     return 0;
 }
@@ -87,16 +101,17 @@ int one_time_cleanup() {
     free(client_chain);
     GUARD(s2n_cert_chain_and_key_free(server_chain_and_key));
     GUARD(s2n_config_free(server_config));
+    GUARD(s2n_connection_free(server_conn));
+    GUARD(s2n_connection_free(client_conn));
 
     return 0;
 }
 
 
-static int configure_server(const struct s2n_ecc_named_curve *curve_to_test, const struct s2n_kem *kem_to_test,
-                            struct s2n_cipher_suite *cipher_suite_to_test) {
-    server_conn->secure.kem_params.kem = kem_to_test;
-    server_conn->secure.cipher_suite = cipher_suite_to_test;
-    server_conn->secure.server_ecc_evp_params.negotiated_curve = curve_to_test;
+static int configure_server_for_test(struct test_case test) {
+    server_conn->secure.kem_params.kem = test.kem_to_test;
+    server_conn->secure.cipher_suite = test.cipher_suite_to_test;
+    server_conn->secure.server_ecc_evp_params.negotiated_curve = test.curve_to_test;
     GUARD(s2n_choose_sig_scheme_from_peer_preference_list(server_conn, &server_conn->handshake_params.client_sig_hash_algs,
                                                           &server_conn->secure.conn_sig_scheme));
     server_conn->handshake_params.our_chain_and_key = server_chain_and_key;
@@ -105,38 +120,22 @@ static int configure_server(const struct s2n_ecc_named_curve *curve_to_test, con
     return 0;
 }
 
-static int configure_client(const struct s2n_ecc_named_curve *curve_to_test, const struct s2n_kem *kem_to_test,
-                           struct s2n_cipher_suite *cipher_suite_to_test) {
-    client_conn->secure.kem_params.kem = kem_to_test;
-    client_conn->secure.cipher_suite = cipher_suite_to_test;
-    client_conn->secure.server_ecc_evp_params.negotiated_curve = curve_to_test;
+static int configure_client_for_test(struct test_case test) {
+    client_conn->secure.kem_params.kem = test.kem_to_test;
+    client_conn->secure.cipher_suite = test.cipher_suite_to_test;
+    client_conn->secure.server_ecc_evp_params.negotiated_curve = test.curve_to_test;
     client_conn->config->security_policy = &security_policy_pq_tls_1_0_2020_12;
-
-    DEFER_CLEANUP(struct s2n_stuffer certificate_in = {0}, s2n_stuffer_free);
-    GUARD(s2n_stuffer_alloc(&certificate_in, S2N_MAX_TEST_PEM_SIZE));
-    DEFER_CLEANUP(struct s2n_stuffer certificate_out = {0}, s2n_stuffer_free);
-    GUARD(s2n_stuffer_alloc(&certificate_out, S2N_MAX_TEST_PEM_SIZE));
-
-    struct s2n_blob temp_blob = {0};
-    GUARD(s2n_blob_init(&temp_blob, (uint8_t *) client_chain, strlen(client_chain) + 1));
-    GUARD(s2n_stuffer_write(&certificate_in, &temp_blob));
-    GUARD(s2n_stuffer_certificate_from_pem(&certificate_in, &certificate_out));
-
-    temp_blob.size = s2n_stuffer_data_available(&certificate_out);
-    temp_blob.data = static_cast<uint8_t *>(s2n_stuffer_raw_read(&certificate_out, temp_blob.size));
-    s2n_pkey_type pkey_type = {};
-    GUARD(s2n_asn1der_to_public_key_and_type(&client_conn->secure.server_public_key, &pkey_type, &temp_blob));
     client_conn->actual_protocol_version = S2N_TLS12;
+
+    s2n_pkey_type temp_pkey_type = {};
+    /* This loads the public key that will be used for verification during the handshake */
+    GUARD(s2n_asn1der_to_public_key_and_type(&client_conn->secure.server_public_key, &temp_pkey_type, &client_asn1der));
     return 0;
 }
 
-static int run_kex(const struct s2n_ecc_named_curve *curve_to_test, const struct s2n_kem *kem_to_test,
-                   struct s2n_cipher_suite *cipher_suite_to_test) {
-
-
-
-    GUARD(configure_server(curve_to_test, kem_to_test, cipher_suite_to_test));
-    GUARD(configure_client(curve_to_test, kem_to_test, cipher_suite_to_test));
+static int run_handshake(struct test_case test) {
+    GUARD(configure_server_for_test(test));
+    GUARD(configure_client_for_test(test));
 
     /* Part 1: Server calls send_key */
     GUARD(s2n_server_key_send(server_conn));
@@ -172,75 +171,12 @@ static int run_kex(const struct s2n_ecc_named_curve *curve_to_test, const struct
     return 0;
 }
 
-static void BM_dh_null(benchmark::State& state) {
-    for (auto _ : state) {
-        BENCHMARK_SUCCESS(run_kex(nullptr, nullptr, &s2n_dhe_rsa_with_aes_256_gcm_sha384));
-    }
-}
+auto BM_test = [](benchmark::State& state, struct test_case test) {
+  for (auto _ : state) {
+      BENCHMARK_SUCCESS(run_handshake(test));
+  }
+};
 
-static void BM_ecdhe_256_null(benchmark::State& state) {
-    for (auto _ : state) {
-        BENCHMARK_SUCCESS(run_kex(&s2n_ecc_curve_secp256r1, nullptr, &s2n_ecdhe_rsa_with_aes_256_gcm_sha384));
-    }
-}
-
-static void BM_ecdhe_384_null(benchmark::State& state) {
-    for (auto _ : state) {
-        BENCHMARK_SUCCESS(run_kex(&s2n_ecc_curve_secp384r1, nullptr, &s2n_ecdhe_rsa_with_aes_256_gcm_sha384));
-    }
-}
-
-static void BM_ecdhe_521_null(benchmark::State& state) {
-    for (auto _ : state) {
-        BENCHMARK_SUCCESS(run_kex(&s2n_ecc_curve_secp521r1, nullptr, &s2n_ecdhe_rsa_with_aes_256_gcm_sha384));
-    }
-}
-
-static void BM_ecdhe_x25519_null(benchmark::State& state) {
-    for (auto _ : state) {
-        BENCHMARK_SUCCESS(run_kex(&s2n_ecc_curve_x25519, nullptr, &s2n_ecdhe_rsa_with_aes_256_gcm_sha384));
-    }
-}
-
-static void BM_ecdhe_256_sike_p503_r1(benchmark::State& state) {
-    for (auto _ : state) {
-        BENCHMARK_SUCCESS(run_kex(&s2n_ecc_curve_secp256r1, &s2n_sike_p503_r1, &s2n_ecdhe_sike_rsa_with_aes_256_gcm_sha384));
-    }
-}
-
-static void BM_ecdhe_256_sike_p434_r2(benchmark::State& state) {
-    for (auto _ : state) {
-        BENCHMARK_SUCCESS(run_kex(&s2n_ecc_curve_secp256r1, &s2n_sike_p434_r2, &s2n_ecdhe_sike_rsa_with_aes_256_gcm_sha384));
-    }
-}
-
-static void BM_ecdhe_256_bike_l1_r1(benchmark::State& state) {
-    for (auto _ : state) {
-        BENCHMARK_SUCCESS(run_kex(&s2n_ecc_curve_secp256r1, &s2n_bike1_l1_r1, &s2n_ecdhe_bike_rsa_with_aes_256_gcm_sha384));
-    }
-}
-
-static void BM_ecdhe_256_bike_l1_r2(benchmark::State& state) {
-    for (auto _ : state) {
-        BENCHMARK_SUCCESS( run_kex(&s2n_ecc_curve_secp256r1, &s2n_bike1_l1_r2, &s2n_ecdhe_bike_rsa_with_aes_256_gcm_sha384));
-    }
-}
-static void BM_ecdhe_256_kyber_512_r2(benchmark::State& state) {
-    for (auto _ : state) {
-        BENCHMARK_SUCCESS(run_kex(&s2n_ecc_curve_secp256r1, &s2n_kyber_512_r2, &s2n_ecdhe_kyber_rsa_with_aes_256_gcm_sha384));
-    }
-}
-
-BENCHMARK(BM_dh_null);
-BENCHMARK(BM_ecdhe_256_null);
-BENCHMARK(BM_ecdhe_384_null);
-BENCHMARK(BM_ecdhe_521_null);
-BENCHMARK(BM_ecdhe_x25519_null);
-BENCHMARK(BM_ecdhe_256_kyber_512_r2);
-BENCHMARK(BM_ecdhe_256_bike_l1_r1);
-BENCHMARK(BM_ecdhe_256_bike_l1_r2);
-BENCHMARK(BM_ecdhe_256_sike_p503_r1);
-BENCHMARK(BM_ecdhe_256_sike_p434_r2);
 
 int main(int argc, char** argv) {
 #if defined(OPENSSL_IS_BORINGSSL)
@@ -250,13 +186,20 @@ int main(int argc, char** argv) {
 #else
     printf("Build with OpenSSL at 0x%lx\n", OPENSSL_VERSION_NUMBER);
 #endif
+
+    for (size_t i = 0; i < s2n_array_len(tests); i++) {
+        struct test_case test = tests[i];
+        std::string cipher_name = test.cipher_suite_to_test == nullptr ? "null" : test.cipher_suite_to_test->name;
+        std::string curve_name = test.curve_to_test == nullptr ? "null" : test.curve_to_test->name;
+        std::string kem_name = test.kem_to_test == nullptr ? "null" : test.kem_to_test->name;
+        std::string benchmark_name = curve_name + "__" + kem_name + "__" + cipher_name;
+        benchmark::RegisterBenchmark(benchmark_name.c_str(), BM_test, test);
+    }
     ::benchmark::Initialize(&argc, argv);
+
     GUARD(s2n_init());
     GUARD(one_time_setup());
-    if(one_time_setup() !=0) {
-        printf("Failed onetime setup");
-        return 1;
-    }
+    GUARD(one_time_setup());
 
     if (::benchmark::ReportUnrecognizedArguments(argc, argv)){
         return 1;
